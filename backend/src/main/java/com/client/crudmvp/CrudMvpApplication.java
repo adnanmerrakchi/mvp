dac967e4d384bbb9c44433092c3c3b05fca4488f
admerra <adnanmerrakchi@gmail.com>
2025-10-21 22:21:41 +0200

    mvp01: init backend project.

---
+++ backend/src/main/java/com/client/crudmvp/CrudMvpApplication.java
@@ -0,0 +1,13 @@
+package com.client.crudmvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMvpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudMvpApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/client/crudmvp/CrudMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.crudmvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMvpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
