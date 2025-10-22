c035bd4d2a756e9c0b4cf393f8c76631571fffa3
admerra <adnanmerrakchi@gmail.com>
2025-10-22 08:59:03 +0200

    mvp06: add insert client feature.

--- backend/src/main/java/com/client/crudmvp/application/dto/ClientOutput.java
+++ backend/src/main/java/com/client/crudmvp/application/dto/ClientOutput.java
@@ -1,10 +1,12 @@
 package com.client.crudmvp.application.dto;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public record ClientOutput(
         UUID id,
         String name,
+        LocalDate dateOfBirth,
         String taxNumber
 ) {
 }
--- backend/src/main/java/com/client/crudmvp/application/usecase/CreateClientUseCase.java
+++ backend/src/main/java/com/client/crudmvp/application/usecase/CreateClientUseCase.java
@@ -35,7 +35,8 @@ public class CreateClientUseCase {
 
         Client savedClient =  clientRepositoryPort.save(client);
 
-        return new ClientOutput(savedClient.getId(), savedClient.getName(), savedClient.getTaxNumber());
+        return new ClientOutput(savedClient.getId(), savedClient.getName(),
+                savedClient.getDateOfBirth(), savedClient.getTaxNumber());
     }
 
 
